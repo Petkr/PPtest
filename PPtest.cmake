ff49d87be13d5b2996dc8b3c6676f86e4662ca73
Peter Fačko <pfacko1@gmail.com>
2021-11-28 17:05:54 +0100

    more test setup done automatically

--- PPtest.cmake
+++ PPtest.cmake
@@ -1,17 +1,21 @@
 cmake_minimum_required(VERSION 3.20)
 
+include(PPcmake)
+
 function(PPtest TARGET_NAME TEST_SOURCES F)
     add_custom_target("${TARGET_NAME}")
 
     foreach(test_source ${TEST_SOURCES})
-        get_filename_component(test "${test_source}" NAME_WE)
+        get_filename_component(_test "${test_source}" NAME_WE)
 
-        add_executable("${test}" "${test_source}")
-        target_include_directories("${test}" PUBLIC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/..")
-        target_link_libraries("${test}" "PPtest")
-        add_test("${test}" "${test}")
-        add_dependencies("${TARGET_NAME}" "${test}")
+        add_executable("${_test}" "${test_source}")
+        target_compile_options("${_test}" PUBLIC "-include" "iostream")
+        target_include_directories("${_test}" PUBLIC "${CMAKE_SOURCE_DIR}" "${PPCMAKE_VENDORDIR}/include")
+        target_link_directories("${_test}" PUBLIC "${PPCMAKE_VENDORDIR}/lib/")
+        target_link_libraries("${_test}" "PPtest")
+        add_test("${_test}" "${_test}")
+        add_dependencies("${TARGET_NAME}" "${_test}")
 
-        cmake_language(CALL "${F}" "${test}")
+        cmake_language(CALL "${F}" "${_test}")
     endforeach()
 endfunction()
